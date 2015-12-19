32b4deabd34b870404354bf2428e9b3c61987a6b
Thomas Mijieux <thomas.mijieux@hotmail.fr>
2015-12-19 11:06:34 +0100

    main prototype can now have envp

--- libu/__start_main.c
+++ libu/__start_main.c
@@ -4,7 +4,8 @@
 
 #include "uimpl.h"
 
-int main(int, struct uuarray *);
+
+int main(int, struct uuarray *, struct uuarray *);
 
 /*
   j'ai remplacé le point d'entré normal du programme (main)
@@ -13,7 +14,18 @@ int main(int, struct uuarray *);
  */
 int __start_main(int argc, char **argv, char **envp)
 {
-    struct uuarray *arr = GC_MALLOC((sizeof*argv) * (argc+2));
-    memcpy(arr, &argv[-1], (sizeof*argv) * (argc+2));
-    return main(argc, arr);
+    int env_size = 0;
+    while (envp[env_size++]);
+
+    struct uuarray *uuargv, *uuenvp;
+    
+    uuargv = GC_MALLOC((sizeof*argv) * (argc+2));
+    memcpy(uuargv, &argv[-1], (sizeof*argv) * (argc+2));
+
+    
+    uuenvp = GC_MALLOC((sizeof*envp) * (env_size+2));
+    memcpy(uuenvp, &envp[0], (sizeof*argv) * (env_size+1));
+    uuenvp->size = (uint64_t) env_size;
+    
+    return main(argc, uuargv, uuenvp);
 }
